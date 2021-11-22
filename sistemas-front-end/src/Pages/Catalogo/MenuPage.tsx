165f0ca29f6a27250a1edce466cc7e538e287ec2
kasvrol <dovelekarolia@gmail.com>
2021-11-22 20:19:27 -0300

    página de inclusão

--- sistemas-front-end/src/Pages/Catalogo/MenuPage.tsx
+++ sistemas-front-end/src/Pages/Catalogo/MenuPage.tsx
@@ -13,15 +13,6 @@ const MenuPage = (): JSX.Element => {
             </Header>
             <Main>
                 <main>
-                    <p>
-                    Tenha acesso a grandes ofertas!! Faça login ou cadastre-se para receber nossas ofertas e promoções!!
-                    </p>
-                    <button>
-                        Cadastre-se
-                    </button>
-                    <button>
-                        Login
-                    </button>
                 </main>
             </Main>
             <Footer>
@@ -39,41 +30,16 @@ export default MenuPage;
 
 const Header = styled(Container)`
 
-    header{
-        background-color: yellow;
-    }
 
 `
 const Main = styled(Container)`
 
-    main{
-        background-color:#484848;
-        display: flex;
-        flex-direction: column;
-        aling-items: center;
-        justify-content: space-around;
-    }
-    p{
-        color: white;
-        font-size: 64px;
-        font-family: Sans Serif;
-    }
-    button{
-        background-color:#C4C4C4;
-        width: 343px;
-        height: 69px;
-        font-family: Droid Sans;
-        font-size: 36px;
-        text-aling: center;
-    }
+   
 
 `
 const Footer = styled(Container)`
 
-    footer{
-        background-color: yellow;
-        font-size: 16px;
-    }
+   
     
 
 `
--- sistemas-front-end/src/Pages/inclusao/InclusaoPage.tsx
+++ sistemas-front-end/src/Pages/inclusao/InclusaoPage.tsx
@@ -13,15 +13,39 @@ const InclusaoPage = (): JSX.Element => {
             </Header>
             <Main>
                 <main>
-                    <p>
-                    Tenha acesso a grandes ofertas!! Faça login ou cadastre-se para receber nossas ofertas e promoções!!
-                    </p>
-                    <button>
-                        Cadastre-se
-                    </button>
-                    <button>
-                        Login
-                    </button>
+                <div className="menu">
+                        <ul>
+                            <li><p>Marcas populares:</p></li>
+                            <a><li>Volksvagem</li></a>
+                            <a><li>Fiat</li></a>
+                            <a><li>Chefrolet</li></a>
+                            <a><li>Peugeot</li></a>
+                            <li><p>Veículos populares:</p></li>
+                            <li><a>Gol 2020</a></li>
+                            <a><li>Strada</li></a>
+                            <a><li>Onix</li></a>
+                            <a><li>Peugeot 308</li></a>
+                            <a><li>Recebimento de veículos</li></a>
+                            <a><li>Inclusão de veículos</li></a>
+                            <a><li>Recebimento de veículos</li></a>
+                        </ul>
+                    </div>
+                    <div className="inclusao">
+                        <section>
+                            imagem
+                            <input  type="text" placeholder="Senha:" />
+                            <button>Adicionar</button>
+                        </section>
+                        <section>
+                        <input  type="text" placeholder="Modelo:" />
+                        <input  type="text" placeholder="Marca:" />
+                        <input  type="text" placeholder="Ano:" />
+                        <input  type="text" placeholder="Combustível" />
+                        <input  type="text" placeholder="Placa" />
+                        <input  type="text" placeholder="Cor:" />
+                        <input  type="text" placeholder="Valor da locação:" />
+                        </section>
+                    </div>
                 </main>
             </Main>
             <Footer>
@@ -45,28 +69,49 @@ const Header = styled(Container)`
 
 `
 const Main = styled(Container)`
-
-    main{
-        background-color:#484848;
-        display: flex;
-        flex-direction: column;
-        aling-items: center;
-        justify-content: space-around;
-    }
-    p{
-        color: white;
-        font-size: 64px;
-        font-family: Sans Serif;
-    }
-    button{
-        background-color:#C4C4C4;
-        width: 343px;
-        height: 69px;
-        font-family: Droid Sans;
-        font-size: 36px;
-        text-aling: center;
-    }
-
+main{
+    background-color:#484848;
+    display: flex;
+    flex-direction: row;
+}
+.menu{
+    background-color:#FCDC34;
+    font-color: #FFFFFF;
+    font-family: Droid Sans;
+    font-size:24px;
+    height: 644px;
+    margin: 40px;
+    text-align: justify;
+}
+.menu li{
+    list-style-type: none;
+    font-color: black;
+    margin-left: 40px;
+    margin-right: 40px;
+}
+.menu p{
+    font-family: Droid Sans;
+    font-size:24px;
+}
+section{
+    display:flex;
+    flex-direction: column;
+}
+.button{
+    background-color: black;
+    color:white;
+    font-size: 24px;
+    height: 34px;
+    width: 121px;
+    margin-bottom: 41px;
+}
+.inclusao{
+    display: flex;
+    flex-direction: row;
+    height: 702px;
+    width:694px;
+    background-color: #C4C4C4;
+}
 `
 const Footer = styled(Container)`
 
